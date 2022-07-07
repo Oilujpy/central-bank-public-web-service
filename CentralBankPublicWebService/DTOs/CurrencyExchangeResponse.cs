78ebc918bdd78df93cf44160dafea1c8821b1ff2
Nancy Campusano <nancycampusano1@gmail.com>
2022-07-07 12:20:36 -0400

    Adding Currency Exchange service

---
+++ CentralBankPublicWebService/DTOs/CurrencyExchangeResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CentralBankPublicWebService.DTOs
+{
+    public class CurrencyExchangeResponse
+    {
+        public decimal DOPConversion { get; set; }
+    }
+}
