9056909535961379960b5dbd8279a767c235d233
Ronald Ogando <ronaldogando150@gmail.com>
2022-07-06 23:18:09 -0400

    Add CreditHistoryResponse

---
+++ CentralBankPublicWebService/DTOs/CreditHistoryResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CentralBankPublicWebService.DTOs
+{
+    public class CreditHistoryResponse
+    {
+        public string DebtorJuridicTaxpayerIdentificationNumber { get; set; }
+        public string DebtConcept { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
